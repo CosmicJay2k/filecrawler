1bda8c6867bd45e13019507bf94c60bdff85ee58
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-08-24 16:07:49 +0200

    Add search through specified file

--- FileCrawler.java
+++ FileCrawler.java
@@ -1,16 +1,27 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class FileCrawler {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
 
+    // User input
     Scanner search = new Scanner(System.in);
-
     System.out.println("What are you looking for?");
-
     String keyword = search.nextLine();
 
-    System.out.println("Looking for " + keyword);
+    // Check file and print result
+    Scanner readFile = new Scanner(new FileReader("./text.txt"));
+    while (readFile.hasNextLine()) {
+      String match = readFile.next();
+      if (match.matches(keyword)) {
+        System.out.println("Yay, found it!");
+      }
+    }
+
+    search.close();
+    readFile.close();
 
   }
 }
